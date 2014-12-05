e2c740cf490976af684d37430ed8a3ef8aeb2129
Thies Gehrmann <t.gehrmann@tudelft.nl>
2014-12-05 10:55:35 +0100

    Initial commit with the statistical test and the cluster cutting algorithm

---
+++ EnrichmentTest.py
@@ -0,0 +1,37 @@
+from scipy.stats import chi2_contingency; 
+
+class Test:
+
+  T      = None;
+  IDS    = None;
+  X      = None;
+  labels = None;
+
+  def __init__(self, T, IDS, X, labels):
+    self.T      = T;
+    self.IDS    = IDS;
+    self.X      = X;
+    self.labels = labels;
+  #edef
+
+  def test(self, i_node, subset):
+    """
+    Do a simple enrichment using the Chi^2 test.
+    """
+    
+    subset_labels = [ self.labels[sample] for sample in sub_set ];
+    
+    a = sum(self.labels);
+    b = sum(subset_labels);
+    
+    c = len(self.labels)   - a;
+    d = len(subset_labels) - b;
+    
+    a += 1; b += 1; c += 1; d += 1;
+    
+    chi2, pvalue, dof, expected = chi2_contingency([[a,b],[c,d]]);
+    
+    return pvalue;
+  #edef
+
+#eclass
--- README.md
+++ README.md
@@ -2,3 +2,15 @@ DT-cut
 ======
 
 Dynamic Test Cut: A general purpose tool to dynamically cut a tree based on a statistical test at each node.
+
+A general purpose implementation of the dynamic tree cutting algorithm in [Proteny](https://github.com/thiesgehrmann/proteny#proteny).
+
+## Dependencies
+
+* Numpy: http://www.numpy.org/
+* Scipy: http://www.scipy.org/
+* Fastcluster: https://pypi.python.org/pypi/fastcluster
+
+## Example usage
+
+
